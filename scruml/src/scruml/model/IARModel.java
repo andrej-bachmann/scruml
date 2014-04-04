d14a75cf1302224fb2045f4650546316ba014581
Kevin Dietrich <hallo@kevin-dietrich.com>
2014-04-04 19:18:36 +0200

    Created IARModel

---
+++ scruml/src/scruml/model/IARModel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scruml.model;
+
+/**
+ *
+ * @author Kevin
+ */
+public interface IARModel {
+    
+    public String getTablename();
+}
