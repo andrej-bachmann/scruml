23084f0952ceaa781d8e99b20e315a31438942be
Kevin Dietrich <hallo@kevin-dietrich.com>
2014-04-04 19:18:59 +0200

    Created IDatabaseController

---
+++ scruml/src/scruml/controller/IDatabaseController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scruml.controller;
+
+import scruml.model.IARModel;
+
+/**
+ *
+ * @author Kevin
+ */
+public interface IDatabaseController {
+    
+    public void connect() throws Exception;
+    public void disconnect() throws Exception;
+    
+    public IARModel find(String modelName, String where) throws Exception;
+    
+}
