9538b15bd2a4ea37e0ff512d7ba41368372ef56f
Thomas Kausler <thomas.kausler@hof-university.de>
2014-04-09 09:09:56 +0200

    Requirement Model created and database extended

---
+++ scruml/src/scruml/model/RequirementModel.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scruml.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import scruml.database.DBSimpleIntegerProperty;
+import scruml.database.DBSimpleStringProperty;
+
+/**
+ *
+ * @author scruml
+ */
+public class RequirementModel implements IARModel{
+    
+    private final IntegerProperty id = new DBSimpleIntegerProperty();
+    private final StringProperty title = new DBSimpleStringProperty();
+    private final StringProperty description = new DBSimpleStringProperty();
+
+    @Override
+    public String getTablename(){
+        return "requirement";
+    }
+
+    @Override
+    public String getKey() {
+        return "id";
+    }
+    
+    
+    public int getId() {
+        return this.id.get();
+    }
+    public void setId(int id){
+        this.id.set(id);
+    }
+    
+    
+    public String getTitle() {
+        return this.title.get();
+    }
+    public void setTitle(String title){
+        this.title.set(title);
+    }
+
+    
+    public String getDescription() {
+        return this.description.get();
+    }
+    public void setDescription(String description){
+        this.description.set(description);
+    }    
+}
