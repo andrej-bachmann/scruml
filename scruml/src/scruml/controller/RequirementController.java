af5a1dcb28fb1f22f474e223eb1ee3607500faa6
Kevin Dietrich <hallo@kevin-dietrich.com>
2014-04-11 23:21:27 +0200

    RequirementController: Created getAllRequirements()

--- scruml/src/scruml/controller/RequirementController.java
+++ scruml/src/scruml/controller/RequirementController.java
@@ -1,9 +1,26 @@
 package scruml.controller;
 
+import java.util.List;
+import scruml.model.IARModel;
+import scruml.model.RequirementModel;
+
 /**
- *
+ * RequirementController handles all the requirement related tasks.
  * @author Simon Deubzer, Kevin Dietrich, Manuel Fachtan, David Goller, Thomas Kausler
  */
 public class RequirementController {
+    
+    /**
+     * This method gets all requirement models from database.
+     * @return List of RequirementModel
+     */
+    public List<IARModel> getAllRequirements() {
+        try {
+            return SQLiteDatabaseController.getInstance().findAll(RequirementModel.class, null);
+        }
+        catch(Exception e) {
+            return null;
+        }
+    }
     
 }
