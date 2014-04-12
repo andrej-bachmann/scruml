356a4922023aca07538e83194b69ff146104d0fa
Thomas Kausler <thomas.kausler@hof-university.de>
2014-04-12 12:27:54 +0200

    Requirement Controller

--- scruml/src/scruml/controller/RequirementController.java
+++ scruml/src/scruml/controller/RequirementController.java
@@ -1,20 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package scruml.controller;
-
 import java.util.List;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import scruml.database.DBSimpleIntegerProperty;
+import scruml.database.DBSimpleStringProperty;
 import scruml.model.IARModel;
+import scruml.view.RequirementViewController;
 import scruml.model.RequirementModel;
-
 /**
- * RequirementController handles all the requirement related tasks.
  * @author Simon Deubzer, Kevin Dietrich, Manuel Fachtan, David Goller, Thomas Kausler
  */
-public class RequirementController {
+public class RequirementController{
     
-    /**
-     * This method gets all requirement models from database.
-     * @return List of RequirementModel
-     */
-    public List<IARModel> getAllRequirements() {
+    private RequirementModel reqMod;
+    IDatabaseController dbController = SQLiteDatabaseController.getInstance();
+    private final IntegerProperty id = new DBSimpleIntegerProperty();
+    private final StringProperty title = new DBSimpleStringProperty();
+    private final StringProperty description = new DBSimpleStringProperty();
+    private final IntegerProperty priority = new DBSimpleIntegerProperty();
+    
+    public void createRequirement(String title, String description)throws Exception{
+        reqMod = new RequirementModel();
+        reqMod.setTitle(this.title.toString());
+        reqMod.setDescription(this.description.toString());
+        dbController.save(reqMod);
+    }
+    
+ //   public void changeRequirementPriority(){
+ //      reqMod.setPriority(priority);
+ //   }
+    
+     public List<IARModel> getAllRequirements() {
         try {
             return SQLiteDatabaseController.getInstance().findAll(RequirementModel.class, null);
         }
@@ -22,5 +44,4 @@ public class RequirementController {
             return null;
         }
     }
-    
 }
