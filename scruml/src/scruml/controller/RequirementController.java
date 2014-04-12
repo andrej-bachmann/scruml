f86a22c471891bc3f67c59e351823b4d8504f9ad
Kevin Dietrich <hallo@kevin-dietrich.com>
2014-04-12 14:22:42 +0200

    Rewrite RequirementController

--- scruml/src/scruml/controller/RequirementController.java
+++ scruml/src/scruml/controller/RequirementController.java
@@ -1,42 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package scruml.controller;
+
 import java.util.List;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
-import scruml.database.DBSimpleIntegerProperty;
-import scruml.database.DBSimpleStringProperty;
 import scruml.model.IARModel;
-import scruml.view.RequirementViewController;
 import scruml.model.RequirementModel;
+
 /**
+ * RequirementController handles all the requirement related tasks.
  * @author Simon Deubzer, Kevin Dietrich, Manuel Fachtan, David Goller, Thomas Kausler
  */
-public class RequirementController{
+public class RequirementController {
     
-    private RequirementModel reqMod;
-    IDatabaseController dbController = SQLiteDatabaseController.getInstance();
-    private final IntegerProperty id = new DBSimpleIntegerProperty();
-    private final StringProperty title = new DBSimpleStringProperty();
-    private final StringProperty description = new DBSimpleStringProperty();
-    private final IntegerProperty priority = new DBSimpleIntegerProperty();
+    private final IDatabaseController db;
     
-    public void createRequirement(String title, String description)throws Exception{
-        reqMod = new RequirementModel();
-        reqMod.setTitle(this.title.toString());
-        reqMod.setDescription(this.description.toString());
-        dbController.save(reqMod);
+    /**
+     * Constructor gets the database controller.
+     */
+    public RequirementController() {
+        this.db = SQLiteDatabaseController.getInstance();
+    }
+    
+    /**
+     * This method creates a new requirement and saves it in the database.
+     * @param title Title of the requirement
+     * @param description Description of the requirement
+     * @return RequirementModel
+     * @throws Exception 
+     */
+    public RequirementModel createRequirement(String title, String description) throws Exception {
+        RequirementModel model = new RequirementModel();
+        model.setTitle(title);
+        model.setDescription(description);
+        model.setPriority(1); //default priority
+        this.db.save(model);
+        return model;
     }
     
  //   public void changeRequirementPriority(){
  //      reqMod.setPriority(priority);
  //   }
     
-     public List<IARModel> getAllRequirements() {
+    /**
+     * This method gets all requirement models from database.
+     * @return List of RequirementModel
+     */
+    public List<IARModel> getAllRequirements() {
         try {
             return SQLiteDatabaseController.getInstance().findAll(RequirementModel.class, null);
         }
